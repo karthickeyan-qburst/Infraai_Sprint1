c59d6174d7d415b921f9ec3aa4bb8dc7dcbcb388
anil rajan <anil.rajan@qburst.com>
2022-04-26 17:50:13 +0530

    added the route to the app file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import * as React from "react";
-import { Box } from '@mui/material';
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
-import Header from './admin/layout/Header/Header';
-import CardComponent from './admin/components/Card/CardComponent';
+import Header from "./admin/layout/Header/Header";
+import CardComponent from "./admin/components/Card/CardComponent";
 
+const Home = lazy(() => import("./admin/pages/home/Home"));
+const Marketplace = lazy(() => import("./admin/pages/maketplace/Marketplace"));
+const Login = lazy(() => import("./frontend/login/Login"));
+const Forgetpassword = lazy(() =>
+  import("./frontend/forgotPassword/ForgotPassword")
+);
 
 function App() {
   return (
-  <Box className='parent-container'>
-    <Header />
-    <CardComponent />
-  </Box>);
+    <>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          {/* Route for users not logged in*/}
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgetpassword" element={<Forgetpassword />} />
+          </Routes>
+
+          {/* Route for users logged in*/}
+          <Box className="parent-container">
+            <Header />
+            <CardComponent />
+          </Box>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/marketplace" element={<Marketplace />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </>
+  );
 }
 
 export default App;
