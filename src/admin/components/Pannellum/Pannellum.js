c87fcbdd2d1803e0e51d52f267135657a85a75bf
kavu2998 <kauvya2998@gmail.com>
2022-05-05 20:36:54 +0530

    Changed images in footage page

--- src/admin/components/Pannellum/Pannellum.js
+++ src/admin/components/Pannellum/Pannellum.js
@@ -6,6 +6,8 @@ import './Pannellum.scss';
 import ReactPannellum from 'react-pannellum'; // toggleFullscreen, //   addHotSpot // getAllScenes, //   addScene, //   getCurrentScene, //   getViewer, //   loadScene, //   getConfig,
 import { Card } from '@material-ui/core';
 
+import Panellum  from '../../../assets/panellum.JPG';
+
 export default function Pannellum({ sceneId, imageSource, height, width }) {
   const [config, setConfig] = useState({
     uiText: { loadingLabel: '' },
@@ -29,12 +31,12 @@ export default function Pannellum({ sceneId, imageSource, height, width }) {
         id="pannellumId"
         sceneId={sceneId || 'scene1'}
         imageSource={
-          imageSource ||
-          'https://images.unsplash.com/photo-1596263576925-d90d63691097?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=kris-guico-rsB-he-ye7w-unsplash.jpg&w=1920'
+          imageSource || Panellum
         }
         config={config}
         className="custom-panell"
       />
+      
     </Card>
   );
 }
