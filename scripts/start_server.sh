3b3f1c44c7fd576d0356bf41c2b7696edbd87454
karthickeyan-qburst <103484229+karthickeyan-qburst@users.noreply.github.com>
2022-05-06 16:25:32 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+sudo systemctl stop nginx
+cd /var/www/html/
+sudo rm -f index.html
+cd /var/www/build/
+sudo npm run build
+cd build/
+sudo cp index.html /var/www/html/
 sudo systemctl start nginx
